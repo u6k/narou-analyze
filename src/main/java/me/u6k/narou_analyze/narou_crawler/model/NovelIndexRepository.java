79bb9a03b138719450fcecc679b6b61f97bf4aa5
u6k <u6k.apps@gmail.com>
2017-04-13 20:43:25 +0900

    リポジトリ・インターフェイスに@Paramアノテーションが不足していたので、追加しました。

--- src/main/java/me/u6k/narou_analyze/narou_crawler/model/NovelIndexRepository.java
+++ src/main/java/me/u6k/narou_analyze/narou_crawler/model/NovelIndexRepository.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NovelIndexRepository extends JpaRepository<NovelIndex, String> {
 
     @Query("select idx from NovelIndex idx where idx.searchDate = :searchDate")
-    List<NovelIndex> findBySearchDate(Date searchDate);
+    List<NovelIndex> findBySearchDate(@Param("searchDate") Date searchDate);
 
 }
