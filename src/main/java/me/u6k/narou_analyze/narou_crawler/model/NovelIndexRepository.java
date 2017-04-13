b2bcc27b3769d2a15199e2bfdf471818704f68f9
u6k <u6k.apps@gmail.com>
2017-04-13 20:33:11 +0900

    検索日付を指定して、Nコードのリストを取得できるようにしました。

--- src/main/java/me/u6k/narou_analyze/narou_crawler/model/NovelIndexRepository.java
+++ src/main/java/me/u6k/narou_analyze/narou_crawler/model/NovelIndexRepository.java
@@ -1,8 +1,15 @@
 
 package me.u6k.narou_analyze.narou_crawler.model;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NovelIndexRepository extends JpaRepository<NovelIndex, String> {
+
+    @Query("select idx from NovelIndex where idx.searchDate = :searchDate")
+    List<NovelIndex> findBySearchDate(Date searchDate);
 
 }
