23270d349d948313507524d4c5171af8ce02c1c2
u6k <u6k.apps@gmail.com>
2019-03-27 17:42:57 +0900

    Changed: Java Maven project to RubyGem project

---
+++ lib/narou_analyze/version.rb
@@ -0,0 +1,3 @@
+module NarouAnalyze
+  VERSION = "0.1.0"
+end
---
+++ spec/narou_analyze_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe NarouAnalyze do
+  it "has a version number" do
+    expect(NarouAnalyze::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
